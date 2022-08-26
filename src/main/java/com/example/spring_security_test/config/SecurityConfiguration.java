9da3a3b124df3202189face04517c50769b9aef5
wintaye <41543902+wintaye@users.noreply.github.com>
2022-08-25 23:55:33 -0500

    antMatchers update

--- src/main/java/com/example/spring_security_test/config/SecurityConfiguration.java
+++ src/main/java/com/example/spring_security_test/config/SecurityConfiguration.java
@@ -53,17 +53,18 @@ public class SecurityConfiguration {
 
     protected void configure(HttpSecurity http) throws Exception {
 
-//        http.authorizeRequests().antMatchers
-//                        ("/registration**", "/js/**", "/css/**", "/img/**")
-//                .permitAll().anyRequest().authenticated()
-//                .and().formLogin().loginPage("/login")
-//                .permitAll().and().logout().invalidateHttpSession
-//                        (true).clearAuthentication(true)
-//                .logoutRequestMatcher
-//                        (new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/login?logout")
-//                .permitAll();
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests().antMatchers
+                //antMatchers allow you to specify which pages can be accessed without authentication
+                        ("/registration", "/js/**", "/css/**", "/img/**", "/login")
+                .permitAll().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login")
+                .permitAll().and().logout().invalidateHttpSession
+                        (true).clearAuthentication(true)
+                .logoutRequestMatcher
+                        (new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+
 
     }
 }
